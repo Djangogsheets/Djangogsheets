c2f59f55b8f7ace98e72def9f3449d1ea39f12c6
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2023-03-04 16:52:28 +0200

    Add files via upload

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -21,3 +21,12 @@ if bottom_image is not None:
     image = Image.open(bottom_image)
     new_image = image.resize((600, 400))
     st.image(new_image)
+
+
+#htp7 = "https://drive.google.com/file/d/1-3YgXi2RF2PqCpRu7YypDyfVwUal2Nab/view?usp=share_link" 
+#st.image(htp7, caption= "The streamlit image from GOOGLE DRIVE", width=800)    
+bottom_image2 = st.file_uploader('"https://drive.google.com/file/d/1-3YgXi2RF2PqCpRu7YypDyfVwUal2Nab/view?usp=share_link"', type='jpg', key=6)
+if bottom_image2 is not None:
+    image = Image.open(bottom_image2)
+    new_image2 = image.resize((200, 100))
+    st.image(new_image2)
