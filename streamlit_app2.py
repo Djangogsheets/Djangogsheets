dac7cb2903bd29ee772ecfe16107dd03adf0e263
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2023-03-04 16:39:27 +0200

    Add files via upload

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -11,7 +11,7 @@ st.title("Gee vir die Dokter se vrou ook Blomme !!!")
 
 st.markdown("Kontak Sirah direk op WHATS APP nr: 000 0000 0000")
 
-image = Image.open('htp7')
+image = Image.open("htp7")
 #image = Image.open(htp7)
 htp7 = "https://drive.google.com/file/d/1-3YgXi2RF2PqCpRu7YypDyfVwUal2Nab/view?usp=share_link" 
 st.image(image, caption= "Inventory Planner", width=800)
