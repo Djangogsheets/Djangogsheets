5ea915acbd716227ce13e568d7ab8998b92720e1
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2023-03-04 11:05:16 +0200

    Add files via upload

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,12 +1,12 @@
 # streamlit_app.py
 import streamlit as st
-from streamlit_faker import get_streamlit_faker
+#from streamlit_faker import get_streamlit_faker
 
 st.title("Hi Daar Sirah Jamien")
 
-st_faker = get_streamlit_faker()
-st_faker.subheader()
-st_faker.markdown()
-st_faker.selectbox()
-st_faker.slider()
-st_faker.map()
+#st_faker = get_streamlit_faker()
+#st_faker.subheader()
+#st_faker.markdown()
+#st_faker.selectbox()
+#st_faker.slider()
+#st_faker.map()
