896f9bb4f499bfa145ce292f2948a4a24b145b62
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2023-03-04 11:03:25 +0200

    Add files via upload

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,5 +1,8 @@
 # streamlit_app.py
+import streamlit as st
 from streamlit_faker import get_streamlit_faker
+
+st.title("Hi Daar Sirah Jamien")
 
 st_faker = get_streamlit_faker()
 st_faker.subheader()
